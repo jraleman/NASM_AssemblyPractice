684d725b836063733d9bb0a4c9a7a780e5379136
jraleman <jraleman@bendiburg.com>
2018-10-31 01:59:22 -0700

    Rename lesson1.s to helloworld.s

---
+++ asm/helloworld.s
@@ -0,0 +1,31 @@
+; helloworld.s
+
+section .data                       ; used for declaring initialized data or constants
+var:                                ; define structure to place variables (optional)
+    .msg   db  'Hello world!', 0Ah  ; assign string message variable
+                                    ; . (dot) used for name convension (structure like)
+                                    ; db means defined bytes
+                                    ; 0Ah is the line feed character
+
+section .text                       ; used for keeping the **actual code**
+    global  _start                  ; must be decalred because of linker (ld)
+
+_start:                             ; tells linker the entry point
+
+    ; function write (
+    mov   rax, 0x02000004           ; system call = 1 (write)
+
+    ; fd, *str, len )
+    mov   rdi, 1                    ; (fd) file descriptor = 1 (STDOUT)
+    mov   rsi, var.msg              ; (*str) move var.msg string memory address
+    mov   rdx, 13                   ; (len) bytes to write (pluse 0Ah character)
+
+    ; {
+    syscall                         ; invoke operating system to do the write
+
+    ; return ([black box thingy])
+    mov   rax, 0x02000001           ; invoke SYS_WRITE (kernel opcode 4)
+    xor   rdi, rdi                  ; exit code 0
+
+    ; }
+    syscall
